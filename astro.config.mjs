0eebe4b8f2315e89c78709e45f195ea14dc6d42e
Samwel Charles <younggeeks101@gmail.com>
2023-10-23 13:12:51 +0300

    Transparent nav fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  experimental: {
-    assets: true,
-  },
+  experimental: {},
 });
