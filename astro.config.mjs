54a37883f4488d517f0933993f9ed01ad9ee7c2c
Samwel Charles <younggeeks101@gmail.com>
2023-10-26 23:05:27 +0300

    Image fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -6,4 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   experimental: {},
+  image: {
+    service: passthroughImageService(),
+  },
 });
